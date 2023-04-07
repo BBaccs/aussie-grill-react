275b488bd0159e845bd50c04d9796279ce5da46a
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-04-07 13:23:37 -0400

    mobile dropdown state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { navItems } from "../data/navItems.js";
 import { socialNavItems } from "../data/socialNavItems.js";
 
+/* Transition animation for mobile nav btn */
+
 class NavBar extends Component {
     constructor(props) {
         super(props);
@@ -9,17 +11,15 @@ class NavBar extends Component {
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick(e) {
-        this.setState({ clicked: true });
+        this.setState(this.state.clicked ? { clicked: false } : { clicked: true });
     }
     render() {
         return (
             <div>
-                <h1>{this.state.clicked ? 'Clicked!!!' : 'Not Clicked'}</h1>
-
-                <div class="py-2 nav-banner text-white">
-                    <div class="d-flex justify-content-center align-items-center">
-                        <span class="mr-2">Follow the Adventure on</span>
-                        <ul class="social-links d-flex mb-1 pl-0">
+                <div className="py-2 nav-banner text-white">
+                    <div className="d-flex justify-content-center align-items-center">
+                        <span className="mr-2">Follow the Adventure on</span>
+                        <ul className="social-links d-flex mb-1 pl-0">
                             {socialNavItems.map((link, index) => {
                                 return (
                                     <li key={index}><a href={link.url}
@@ -29,43 +29,43 @@ class NavBar extends Component {
                         </ul>
                     </div>
                 </div>
-
-                <div class="sticky-top nav-border">
-                    <nav class="navbar navbar-dark bg-ag-dark">
-                        <div class="container-lg d-flex nav-inner-wrapper">
-                            <button onClick={this.handleClick} class="navbar-toggler d-md-none d-lg-none d-xl-none" type="button" data-toggle="collapse"
-                                data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
+                <div className="sticky-top nav-border">
+                    <nav className="navbar navbar-dark bg-ag-dark">
+                        <div className="container-lg d-flex nav-inner-wrapper">
+                            {/*When the transition animation is added we'll need to add classes like 'collapse' to the btn, which temporarily hides it during transition*/}
+                            <button onClick={this.handleClick} className={this.state.clicked ? "navbar-toggler d-md-none d-lg-none d-xl-none" : "navbar-toggler d-md-none d-lg-none d-xl-none collapsed"} type="button"
+                                data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+                                aria-expanded={this.state.clicked ? 'true' : 'false'} aria-label={this.state.clicked ? 'Close navigation' : 'Open navigation'} >
+                                <span className="navbar-toggler-icon"></span>
                             </button>
-                            <ul class="nav nav-uncollapsed ag-nav">
+                            <ul className="nav nav-uncollapsed ag-nav">
                                 {navItems.map((link, index) => {
                                     return (
                                         <li key={index} class={link.liClass}>
                                             <a className={link.anchorClass} href={link.url}>{link.name}</a>
                                         </li>
-                                    )
-                                })};
+                                    );
+                                })}
                             </ul>
                             <a aria-label="homepage" href="index.html">
                                 <picture>
                                     <source srcSet="assets/other/mobile/agLogoLarge.png" media="(min-width: 1024px)" />
-                                    <img class="ml-2 ag-logo" src="assets/other/mobile/agLogo.png"
+                                    <img className="ml-2 ag-logo" src="assets/other/mobile/agLogo.png"
                                         alt="Aussie Grill By Outback Steakhouse" />
                                 </picture>
                             </a>
                         </div>
                     </nav>
-                    <div class="collapse" id="navbarToggleExternalContent">
-                        <div class="bg-ag-dark p-4">
-                            <ul class="nav d-flex flex-column">
+                    <div className={this.state.clicked ? "collapse show" : "collapse"} id="navbarToggleExternalContent">
+                        <div className="bg-ag-dark p-4">
+                            <ul className="nav d-flex flex-column">
                                 {navItems.map((link, index) => {
                                     return (
-                                        <li key={index} class="nav-item">
+                                        <li key={index} className="nav-item">
                                             <a className={link.anchorClass} href={link.url}>{link.name}</a>
                                         </li>
-                                    )
-                                })};
+                                    );
+                                })}
                             </ul>
                         </div>
                     </div>
