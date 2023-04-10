184a1eef89bce79388688dc51563f5c2f701d7b9
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-04-10 18:20:24 -0400

    add row for menuItems, remove dummy data

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -12,37 +12,44 @@ class MenuItem extends Component {
     //     this.setState(this.state.clicked ? { clicked: false } : { clicked: true });
     // }
     render() {
-        return (
-            <div className="menuItems">
-                {menuCategoryData.map((menuItem, index) => {
-                    return (
-                        /* <!-- {menuItem.name} Menu Item--> */
-                        <div key={index} class="col-lg-4 col-md-6 pt-lg-0 menu-item-col">
-                            <a class="d-block" href={menuItem.url}>
-                                <div class="primary-subheading-wrapper">
-                                    <div class="w-100">
-                                        <div  class="CHANGEMELATER mb-0 primary-subheading">{menuItem.name}</div>
-                                    </div>
-                                </div>
-                                <div>  
-                                    <picture>
-                                        <source
-                                            srcSet={`/assets/foodImages/categoryImages/desktop/${menuItem.img}500x375.${menuItem.imgType}`}
-                                            media="(min-width: 768px)" />
-                                        <img class="w-100 menu-item-img"
-                                            srcSet={`/assets/foodImages/categoryImages/mobile/${menuItem.img}345x260.${menuItem.imgType}`}
-                                            alt="" />
-                                    </picture>
-                                </div>
-                            </a>
+        const menuItems = menuCategoryData.map((menuItem, index) => (
+        /* <!-- Menu Item --> */
+            <div key={index} class="col-lg-4 col-md-6 pt-lg-0 menu-item-col">
+                <a class="d-block" href={menuItem.url}>
+                    <div class="primary-subheading-wrapper">
+                        <div class="w-100">
+                            <div class="CHANGEMELATER mb-0 primary-subheading">{menuItem.name}</div>
                         </div>
-                    )
-                })}
+                    </div>
+                    <div>
+                        <picture>
+                            <source
+                                srcSet={`/assets/foodImages/categoryImages/desktop/${menuItem.img}500x375.${menuItem.imgType}`}
+                                media="(min-width: 768px)" />
+                            <img class="w-100 menu-item-img"
+                                srcSet={`/assets/foodImages/categoryImages/mobile/${menuItem.img}345x260.${menuItem.imgType}`}
+                                alt="" />
+                        </picture>
+                    </div>
+                </a>
+            </div>
+        ));
+        const rows = [];
+        console.log(menuItems)
+        for (let i = 0; i < menuItems.length; i += 3) {
+            rows.push(
+                <div key={i} className="row px-lg-4 justify-content-center">
+                  {menuItems.slice(i, i + 3)}
+                </div>
+              );
+        }
+        return (
+            // <!-- Start Menu Item Row-->
+            <div >
+                {rows}
             </div>
         );
     }
 }
 
 export default MenuItem;
-
-
