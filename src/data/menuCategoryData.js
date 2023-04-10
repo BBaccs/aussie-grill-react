9b0587f23fd290bdd6a453b656d5bc3013cead0c
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-04-10 10:26:08 -0400

    add fake data, test

--- src/data/menuCategoryData.js
+++ src/data/menuCategoryData.js
@@ -48,5 +48,48 @@ export const menuCategoryData = [
         imgType: 'jpg',
         url: "/pages/mainMenu/menuCategories/dessertsAndBeverages.html",
         imgFilePath: "foodImages/categoryImages",
+    },
+    {
+        name: "111",
+        img: "crispyChickenSrirachaSandwich",
+        imgType: 'jpg',
+        url: "/pages/mainMenu/menuCategories/handhelds.html",
+        imgFilePath: "foodImages/categoryImages"
+        // sourceSrc: "nav-item"
+    },
+    {
+        name: "222 Pl222ates",
+        img: "slicedSteakPlatter",
+        imgType: 'jpg',
+        url: "/pages/mainMenu/menuCategories/largePlates.html",
+        imgFilePath: "foodImages/categoryImages",
+    },
+    {
+        name: "Sala33ds",
+        img: "appleArugulaSaladChicken",
+        imgType: 'jpg',
+        url: "/pages/mainMenu/menuCategories/salads.html",
+        imgFilePath: "foodImages/categoryImages",
+    },
+    {
+        name: "Sid645654s",
+        img: "augVolcanoShrimp",
+        imgType: 'jpg',
+        url: "/pages/mainMenu/menuCategories/augVolcanoShrimp.html",
+        imgFilePath: "foodImages/categoryImages",
+    },
+    {
+        name: "Ki654465s",
+        img: "crispyChickenTenders",
+        imgType: 'jpg',
+        url: "/pages/mainMenu/menuCategories/crispyChickenTenders.html",
+        imgFilePath: "foodImages/categoryImages",
+    },
+    {
+        name: "654 & Be645verages",
+        img: "oreoAndToastedPecanSundae",
+        imgType: 'jpg',
+        url: "/pages/mainMenu/menuCategories/dessertsAndBeverages.html",
+        imgFilePath: "foodImages/categoryImages",
     }
 ];
