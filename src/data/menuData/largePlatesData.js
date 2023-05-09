d4480083ec40471ac225a605a24c3dd58e9c2133
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-05-09 13:32:40 -0400

    add new menu data

---
+++ src/data/menuData/largePlatesData.js
@@ -0,0 +1,26 @@
+export const largePlatesData = [
+    {
+      name: "CRISPY CHICKEN TENDERS",
+      img: "crispyChickenTenders",
+      src: "/pages/mainMenu/largePlates/crispyChickenTenders.html",
+      imgFilePath: "foodImages/productImages/largePlates",
+      pdpDescription: "Hand-breaded, crispy seasoned chicken tenders, with house-made honey mustard & choice of side.",
+      pdpPreview: 'Best in Tampa'
+    },
+    {
+      name: "TOPPED GRILLED CHICKEN",
+      img: "toppedGrilledChicken",
+      src: "/pages/mainMenu/handhelds/toppedGrilledChicken.html",
+      imgFilePath: "foodImages/productImages/largePlates",
+      pdpDescription: "Grilled chicken breast, melted cheese, crispy bacon, caramelized onions & choice of side.",
+    },
+    {
+      name: "CENTER-CUT SIRLOIN",
+      img: "sirloin",
+      src: "/pages/mainMenu/handhelds/sirloin.html",
+      imgFilePath: "foodImages/productImages/largePlates",
+      pdpDescription: "7oz sirloin, seasoned and seared, arugula garnish & choice of side.",
+      new: true
+    }
+  ];
+  
---
+++ src/data/menuData/saladsData.js
@@ -0,0 +1,19 @@
+export const saladsData = [
+    {
+      name: "CRISPY CHICKEN TENDERS",
+      img: "AUSSIE SALAD",
+      src: "/pages/mainMenu/salads/aussieSalad.html",
+      imgFilePath: "foodImages/productImages/salads",
+      pdpDescription: "Greens, arugula, cucumbers, onions, tomatoes, and eggs. Served with choice of ranch, creamy Italian, or balsamic vinaigrette dressing.",
+      pdpDescription2: 'Choose your protein: Grilled or Crispy Chicken | Crispy Shrimp.'
+    },
+    {
+      name: "APPLE ARUGULA SALAD",
+      img: "appleArugulaSalad",
+      src: "/pages/mainMenu/salads/appleArugulaSalad.html",
+      imgFilePath: "foodImages/productImages/salads",
+      pdpDescription: "Arugula, apples, carrots, cucumbers, house-made pickles, pumpkin seeds and tomatoes tossed in creamy Italian dressing.",
+      pdpDescription2: "Choose your protein: Grilled or Crispy Chicken | Crispy Shrimp."
+    }
+  ];
+  
