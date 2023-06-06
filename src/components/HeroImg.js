ed6704a1123a6663d0db7a864890b60786adf127
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-06-06 11:26:02 -0400

    hero component fix

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 
 class HeroImg extends Component {
+    static defaultProps = {
+        imgFilePath: "other",
+    };
     render() {
         return (
             <div>
                 <picture>
-                    <source src={`/assets/other/desktop/desktop-${this.props.img}`} media="(min-width: 1400px)" />
-                    <img src={`/assets/other/mobile/mobile-${this.props.img}`} className="d-block w-100"
+                    <source srcSet={`/assets/${this.props.imgFilePath}/desktop/desktop-${this.props.img}`} media="(min-width: 1400px)" />
+                    <source srcSet={`assets/${this.props.imgFilePath}/desktop/tablet-${this.props.img}.jpg`} media="(min-width: 600px)" />
+                    <img srcSet={`/assets/${this.props.imgFilePath}/mobile/mobile-${this.props.img}`} className="d-block w-100" media="(max-width: 1400px)"
                         style={{maxHeight: '700px'}} alt="" />
                 </picture>
+                {/* <picture>
+                <source srcSet={`assets/other/desktop/desktop-agOurStory.jpg`} media="(min-width: 1400px)" />
+                <source srcSet={`assets/other/desktop/ourStory1024x500.jpg`} media="(min-width: 600px)" />
+                <img srcSet={`assets/other/mobile/mobile-agOurStory.jpg`} className="d-block w-100" alt="" />
+            </picture> */}
             </div>
         );
     }
 }
-
 export default HeroImg;
