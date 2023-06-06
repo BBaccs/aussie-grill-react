d4a4c8460ee26428365681740deab45b5ca6f0fb
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-06-06 15:58:26 -0400

    footer links fp

--- src/data/footerItemsSecondary.js
+++ src/data/footerItemsSecondary.js
@@ -1,32 +1,32 @@
 export const footerItemsSecondary = [
     {
         name: 'Privacy Policy',
-        url: '/pages/otherPages/privacyPolicyPages/privacyPolicy.html',
+        url: '/privacyPolicyPages/privacyPolicy.html',
         anchorClass: "nav-link",
         liClass: "nav-item"
       },
       {
         name: 'CCPA Privacy Notice',
-        url: '/pages/otherPages/privacyPolicyPages/ccpa.html',
+        url: '/privacyPolicyPages/ccpa.html',
         anchorClass: "nav-link",
         liClass: "nav-item"
       },
       {
         name: 'Accessability Statement',
-        url: '/pages/otherPages/privacyPolicyPages/accessabilityStatement.html',
+        url: '/privacyPolicyPages/accessabilityStatement.html',
         anchorClass: "nav-link",
         liClass: "nav-item"
       },
       {
         name: 'Terms & Conditions',
-        url: '/pages/otherPages/privacyPolicyPages/termsAndConditions.html',
+        url: '/privacyPolicyPages/termsAndConditions.html',
         anchorClass: "nav-link",
         liClass: "nav-item"
       },
       /* Mobile Only */
       {
         name: 'Giveaways',
-        url: '/pages/giveaway.html',
+        url: '/giveaway.html',
         anchorClass: "nav-link",
         liClass: "nav-item mobile-only"
       }
