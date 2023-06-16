590796b946ca7fb20479d82e1397d42640f6eaad
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-06-16 11:12:56 -0400

    add keyboard functionality

--- src/components/Ordering.js
+++ src/components/Ordering.js
@@ -24,7 +24,11 @@ class Ordering extends Component {
         document.querySelector(`#${e.target.id}`).toggleAttribute('checked')
     }
 
-
+    handleKeyDown = (e, toggleFunc) => {
+        if (e.key === "Enter") {
+            toggleFunc();
+        }
+    }
 
     render() {
         const uniqueStates = [...new Set(locationsData.map((location) => location.stateName))];
@@ -51,7 +55,8 @@ class Ordering extends Component {
                                 checked={this.state.showPickup}
                                 onChange={this.toggleChecked}
                             />
-                            <label tabIndex="0" htmlFor="pickupChk">PickUp</label>
+                            {/* OnKeydown goes on label */}
+                            <label onKeyDown={(e) => this.handleKeyDown(e, this.togglePickup) } tabIndex="0" htmlFor="pickupChk">PickUp</label>
                         </div>
                         <div>
                             <input
@@ -62,7 +67,8 @@ class Ordering extends Component {
                                 type="checkbox"
                                 id="deliveryChk"
                             />
-                            <label tabIndex="0" htmlFor="deliveryChk">Delivery</label> {console.log('this.state.showDelivery', this.state.showDelivery)}
+                            {/* OnKeydown goes on label */}
+                            <label onKeyDown={(e) => this.handleKeyDown(e, this.toggleDelivery)} tabIndex="0" htmlFor="deliveryChk">Delivery</label>
                         </div>
                     </div>
                 </div>
@@ -72,56 +78,3 @@ class Ordering extends Component {
     }
 }
 export default Ordering;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { locationsData } from "../data/locationsData.js";
-// import AModal from "../components/AModal";
-
-// export default function OrderPage() {
-//     const uniqueStates = [...new Set(locationsData.map((location) => location.stateName))];
-
-//     return (
-//         <>
-//             <h1 className="landing-heading mb-4 mb-lg-4">Choose your location to place an order!</h1>
-//             <div id="location-filter-wrapper" className="mt-md-4 mx-auto pickup-layout">
-//                 <div className="mb-2">
-//                     <label id="state-label" className="mb-0" htmlFor="statedd">State :</label>
-//                     <select id="statedd" className="primary-color select">
-//                         <option className="slim-option">All</option>
-//                         {uniqueStates.map((stateName) => (
-//                             <option className="slim-option" key={stateName}>{stateName}</option>
-//                         ))}
-//                     </select>
-//                 </div>
-//                 <div id="checkbox-wrapper" className="mb-3">
-//                     <input onClick={togglePickup} className="option-filter checked-option checkboxLeft" type="checkbox" id="pickupChk" checked />
-//                     <label tabIndex="0" htmlFor="pickupChk">PickUp</label>
-//                     <input onClick={toggleDelivery}  className="option-filter checked-option checkboxRight primary-color" type="checkbox" id="deliveryChk" checked />
-//                     <label tabIndex="0" htmlFor="deliveryChk">Delivery</label>
-//                 </div>
-//             </div>
-//             <div id="location-result" className="mt-md-4 mx-auto pickup-layout">
-//                 <AModal />
-//             </div>
-//         </>
-//     );
-// }
