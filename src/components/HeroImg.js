bb01fc8ada247604c4c100ceb7ae0f94738e9bea
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-10-13 14:45:24 -0400

    destructure

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -6,13 +6,14 @@ class HeroImg extends Component {
         alt: "",
     };
     render() {
+        const { img, imgFilePath, alt } = this.props;
         return (
             <div>
                 <picture loading="lazy">
-                    <source srcSet={`/assets/${this.props.imgFilePath}/desktop/desktop-${this.props.img}`} media="(min-width: 1400px)" />
-                    <source srcSet={`assets/${this.props.imgFilePath}/desktop/tablet-${this.props.img}`} media="(min-width: 600px)" />
-                    <img srcSet={`/assets/${this.props.imgFilePath}/mobile/mobile-${this.props.img}`} className="d-block w-100" 
-                    media="(max-width: 1400px)" style={{maxHeight: '700px'}} alt={this.props.alt} />
+                    <source srcSet={`/assets/${imgFilePath}/desktop/desktop-${img}`} media="(min-width: 1400px)" />
+                    <source srcSet={`assets/${imgFilePath}/desktop/tablet-${img}`} media="(min-width: 600px)" />
+                    <img srcSet={`/assets/${imgFilePath}/mobile/mobile-${img}`} className="d-block w-100" 
+                        media="(max-width: 1400px)" style={{maxHeight: '700px'}} alt={alt} />
                 </picture>    
             </div>
         );
