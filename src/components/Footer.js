c4bed8ca0edcc81e189f70f769af6ca2e571d600
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-10-14 13:38:17 -0400

    refactor footer p2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,44 +9,8 @@ class Footer extends Component {
         let year = new Date().getFullYear();
         return (
             <footer className="footer-outer-wrapper">
-                <div className="footer-wrapper px-3 py-4 bg-ag-dark text-white">
-                    {/* <div className="m-auto d-lg-flex d-none justify-content-between footer-grid">
-                        <div>
-                            <ul className="nav nav-uncollapsed main-links">
-                                {footerItemsDataMain.map((link, index) => {
-                                    return (
-                                        <li className={link.liClass} key={index}>
-                                            <a className={link.anchorClass} href={link.url} target={link.target}>{link.name}</a>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                            <ul className="nav nav-uncollapsed privacy-links">
-                                {footerItemsSecondary.map((link, index) => {
-                                    return (
-                                        <li className={link.liClass} key={index}>
-                                            <a className={link.anchorClass} href={link.url} target={link.target}>{link.name}</a>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </div>
-                        <div className="mt-n-10">
-                            <ul className="social-links d-flex text-center flex-row justify-content-end m-0 p-0 w-100 h-auto">
-                                {socialNavItems.map((link, index) => {
-                                    return (
-                                        <li className={"m-2"} key={index}><a href={link.url}
-                                            className={link.anchorClass} aria-label={'Aussie Grill ' + link.ariaLabel} target={link.target}></a></li>
-                                    );
-                                })}
-                            </ul>
-                            <div><span className="rights-reserved text-capitalize text-right w-100 font-small"> &#169;{year} Bloomin'
-                                Brands, Inc. All Rights Reserved.</span></div>
-                        </div>
-                    </div> */}
-
-
-                    <div className="d-grid footer-grid">
+                <div className="footer-wrapper px-3 py-4 bg-ag-dark">
+                    <div className="footer-grid">
                         <ul className="nav nav-uncollapsed hide-mobile main-links">
                             {footerItemsDataMain.map((link, index) => {
                                 return (
@@ -56,7 +20,7 @@ class Footer extends Component {
                                 );
                             })}
                         </ul>
-                        <ul className="nav nav-uncollapsed d-grid g-second privacy-links">
+                        <ul className="nav nav-uncollapsed g-second privacy-links">
                             {footerItemsSecondary.map((link, index) => {
                                 return (
                                     <li className={link.liClass} key={index}>
@@ -68,22 +32,16 @@ class Footer extends Component {
                         <ul className="social-links m-0 p-0 w-100 h-100 d-flex text-center flex-column g-third">
                             {socialNavItems.map((link, index) => {
                                 return (
-                                    <li className={"m-2"} key={index}><a href={link.url}
+                                    <li key={index}><a href={link.url}
                                         className={link.anchorClass} aria-label={link.ariaLabel} target={link.target}></a></li>
                                 );
                             })}
                         </ul>
-                        <div className="hide-mobile footer-text">
-                            <span className="rights-reserved text-capitalize w-100 font-small"> &#169;{year} Bloomin'
+                        <div className="footer-text">
+                            <span className="rights-reserved"> &#169;{year} Bloomin'
                                 Brands, Inc. All Rights Reserved.
                             </span>
                         </div>
-                    </div>
-                    <div className="mt-3 d-lg-none">
-                        <span className="rights-reserved text-capitalize text-center w-100 font-small">
-                            &#169;{year} Bloomin' Brands, Inc.
-                            All Rights Reserved.
-                        </span>
                     </div>
                 </div>
             </footer>
