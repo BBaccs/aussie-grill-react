37ed03ffa78d7a83fd62fb1bc6595f2d5b67d752
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-10-15 16:45:11 -0400

    create new hook

---
+++ src/hooks/OrderButton.js
@@ -0,0 +1,15 @@
+import { useLocation, Link } from 'react-router-dom';
+
+function OrderButton() {
+    const location = useLocation();
+    return (
+        <>
+            {location.pathname.includes('catering')
+                ? <a className="btn btn-primary btn-lg mr-5" href="https://www.ezcater.com/brand/pvt/aussie-grill"
+                    target="_blank" title="Opens in a new tab" rel="noopener noreferrer">Order Catering</a>
+                : <Link className="btn btn-primary btn-lg mr-5" to="/pickup">Order</Link>}
+        </>
+    );
+}
+
+export default OrderButton;
