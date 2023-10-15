ca0d9d622fee50055e2933c7dd40286ea0220173
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-10-15 19:07:40 -0400

    fix file

---
+++ src/data/cateringMenuData/cateringCategoryData.js
@@ -0,0 +1,20 @@
+export const cateringCategoryData = [
+    {
+        name: "Platters",
+        img: "baconBombBurger.jpeg",
+        imgFilePath: "/assets/foodImages/categoryImages",
+        linkTo: "/catering/platters",
+    },
+    {
+        name: "Salad Platters",
+        img: "saladPlatters.jpeg",
+        imgFilePath: "/assets/foodImages/categoryImages",
+        linkTo: "/catering/saladPlatters",
+    },
+    {
+        name: "Desserts & sides",
+        img: "aussiePetals.jpeg",
+        imgFilePath: "/assets/foodImages/categoryImages",
+        linkTo: "/catering/desserts&Sides",
+    }
+];
