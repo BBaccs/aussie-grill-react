0f3bfe50b0ca3aa3c5470f5d9a954c76f14e8de0
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2023-10-15 20:25:30 -0400

    hide on order page

--- src/hooks/CtaBanner.js
+++ src/hooks/CtaBanner.js
@@ -4,13 +4,17 @@ import OrderButton from "./OrderButton.js";
 function CtaBanner() {
     const location = useLocation();
     return (
-        <div className="cta-wrapper">
-            <div className="d-flex container-fluid justify-content-between align-items-center cta-banner">
-                <img loading="lazy" className="bull-cta"
-                    src={process.env.PUBLIC_URL + "/assets/agGraphicElements/animals/mobile/bullFullSmall.png"} alt="" />
-                    < OrderButton />
-            </div>
-        </div>
+        <>
+            {!location.pathname.includes('pickup') ?
+                <div className="cta-wrapper">
+                    <div className="d-flex container-fluid justify-content-between align-items-center cta-banner">
+                        <img loading="lazy" className="bull-cta"
+                            src={process.env.PUBLIC_URL + "/assets/agGraphicElements/animals/mobile/bullFullSmall.png"} alt="" />
+                        <OrderButton />
+                    </div>
+                </div> : null
+            }
+        </>
     );
 }
 
