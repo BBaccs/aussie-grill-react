1bf41cd6939bb59c5135e9a324f39ad646fec264
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-06-17 13:52:27 -0400

    delete index.js

--- src/functionalComponents/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ACarousel } from '../components/ACarousel';
-export { default as CtaBanner } from '../components/CtaBanner';
-export { default as LocationCardModal } from '../components/LocationCardModal';
-export { default as OrderButton } from '../components/OrderButton';
-export { default as PDP } from '../components/PDP';
